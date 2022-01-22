dce5106778c5475080efb6a2d209ea1fc3cd10e0
thehouseplant <sean.collins@outlook.com>
2022-01-21 18:02:09 -0800

    Add more relevant Hacker News link placeholders

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="flex justify-between">
-    <div class="px-2 mx-4">
-      <div class="hidden lg:flex">
-        <nuxt-link to="/" class="btn btn-ghost btn-sm rounded-btn">
-          Home
-        </nuxt-link>
+  <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+    <div class="flex-none px-2 mx-2">
+      <span class="text-lg font-bold">
+        Hacker Nuews
+      </span>
+    </div>
+    <div class="flex-1 px-2 mx-2">
+      <div class="items-stretch hidden lg:flex">
+        <a class="btn btn-ghost btn-sm rounded-btn">
+          Top
+        </a>
+        <a class="btn btn-ghost btn-sm rounded-btn">
+          New
+        </a>
+        <a class="btn btn-ghost btn-sm rounded-btn">
+          Show
+        </a>
+        <a class="btn btn-ghost btn-sm rounded-btn">
+          Ask
+        </a>
+        <a class="btn btn-ghost btn-sm rounded-btn">
+          Jobs
+        </a>
       </div>
     </div>
   </div>
