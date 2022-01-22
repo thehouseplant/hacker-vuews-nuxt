647d3648bb6cde34c9826953f208c3527c8621e1
thehouseplant <sean.collins@outlook.com>
2022-01-21 17:44:03 -0800

    Build out simple navigation bar

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-between">
+    <div class="px-2 mx-4">
+      <div class="hidden lg:flex">
+        <nuxt-link to="/" class="btn btn-ghost btn-sm rounded-btn">
+          Home
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
