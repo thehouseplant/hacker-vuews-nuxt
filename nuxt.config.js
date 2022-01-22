a94c842ccdfb21caf5bcca774c2f5f09824884e4
thehouseplant <sean.collins@outlook.com>
2022-01-21 18:32:43 -0800

    Update site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hacker-vuews',
+    title: 'Hacker Vuews',
     htmlAttrs: {
       lang: 'en'
     },
