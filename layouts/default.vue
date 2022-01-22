55dede993321317962cb393a3e0e8c3e4b36aa64
thehouseplant <sean.collins@outlook.com>
2022-01-21 17:43:41 -0800

    Create default layout with Navbar placeholder

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'DeFault',
+  components: { Navbar }
+}
+</script>
